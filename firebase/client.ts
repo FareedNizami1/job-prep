24c689d16957df4fbd6d4d610ad86042cb76e80c
FareedNizami1 <fareedalam64@gmail.com>
2025-05-18 16:06:51 -0400

    Setup Firebase

---
+++ firebase/client.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBS-XYebzVd6pHT7_hBzaVOf3PZfQGUisg",
+    authDomain: "jobprep-f862b.firebaseapp.com",
+    projectId: "jobprep-f862b",
+    storageBucket: "jobprep-f862b.firebasestorage.app",
+    messagingSenderId: "573827499683",
+    appId: "1:573827499683:web:23e87c6c4974f28378a116",
+    measurementId: "G-R6CS3S9LYD"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
